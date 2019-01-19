095358d981df80c086e0ed6affc749d36f2f1e1e
unknown <maverickf.fengys@gmail.com>
2019-01-19 14:27:36 +0800

    Initialize a glfw window.

--- opengl/opengl-examples/opengl-examples/opengl_window.cpp
+++ opengl/opengl-examples/opengl-examples/opengl_window.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 
-// 
+/*
+* You must include glad.h before glfw3.h in that glad.h can promise include the correct version of OpenGL header files.
+*/
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello OpenGL" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "An OpenGL Context Window", NULL, NULL);
+	if (NULL == window) {
+		std::cout << "Failed to create a glfw window." << std::endl;
+		glfwTerminate();
+	}
 	return 0;
 }
