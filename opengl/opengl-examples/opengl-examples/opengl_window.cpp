f38038eff1de06f3aba2e41b9077140c4fdd64ac
unknown <maverickf.fengys@gmail.com>
2019-01-29 11:54:23 +0800

    Set the viewport in the OpenGL context.

--- opengl/opengl-examples/opengl-examples/opengl_window.cpp
+++ opengl/opengl-examples/opengl-examples/opengl_window.cpp
@@ -74,7 +74,13 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	
-
+	/*
+	* The mapping between the normalized coordinate and window coordinate is :
+	* x(w) = x + witdth/2 + x(nd)*witdth/2 
+	* y(w) = y + height/2 + y(nd)*height/2 
+	* 
+	*/
+	glViewport(0, 0, 1024, 768);
 	glfwTerminate();
 	return 0;
 }
