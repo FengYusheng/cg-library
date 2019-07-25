4391439101657cd352ee05b508bf756a3d604ae1
雨生 冯 <maverick.fengys@outlook.com>
2019-07-25 17:00:27 +0800

    hello-triangle: get context info, add shaders into a header file

--- opengl/opengl-examples/hello-triangle/hello-triangle.cpp
+++ opengl/opengl-examples/hello-triangle/hello-triangle.cpp
@@ -3,9 +3,13 @@
 
 #include <iostream>
 
+#include "shaders.h"
+
 // settings
 const unsigned int SCR_WIDTH = 1366;
 const unsigned int SCR_HEIGHT = 768;
+
+void framebuffer_size_callback(GLFWwindow*, int, int);
 
 int main(int argc, char* argv[])
 {
@@ -53,8 +57,44 @@ int main(int argc, char* argv[])
 		glfwTerminate();
 		return -1;
 	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	/* Load OpenGL function pointer to this window's context.
+	* https://glad.dav1d.de/
+	*/
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Load OpenGL functions with glfwGetProcesAddress." << std::endl;
+		printf("The OpenGL version loaded is %d, %d\n", GLVersion.major, GLVersion.minor);
+	}
+	else if (gladLoadGL())
+	{
+		std::cout << "Load OpenGL functions with gladLoadGL." << std::endl;
+		printf("The OpenGL version loaded is %d, %d\n", GLVersion.major, GLVersion.minor);
+	}
+	else
+	{
+		std::cout << "Failed to load OpenGL functions." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	/*Get current OpenGL context version with glGetString*/
+	printf("The version of current OpenGL context is %s\n", glGetString(GL_VERSION));
+	printf("The vendor of current OpenGL context is %s\n", glGetString(GL_VENDOR));
+	printf("The renderer of current OpenGL context is %s\n", glGetString(GL_RENDERER));
+	printf("Then shading language version of current OpenGL context is %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+
+
+	//system("pause");
 	glfwTerminate();
 	
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
