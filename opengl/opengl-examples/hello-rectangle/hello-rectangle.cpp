d6ad3e290ebb78c14711d78888ab84c232556141
YushengFeng01 <Yusheng.Feng@clarivate.com>
2019-11-03 14:49:57 +0800

    initialize hello-rectangle

--- opengl/opengl-examples/hello-rectangle/hello-rectangle.cpp
+++ opengl/opengl-examples/hello-rectangle/hello-rectangle.cpp
@@ -1,4 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "shaders.h"
+
+
+
 
 int main(int argc, char* argv[]) {
+	int major = -1;
+	int minor = -1;
+	int rev = -1;
+
+	/*Initialization Hints
+	* glfwInit()
+	*/
+
+	/*Initialize OpenGL context.*/
+	if (!glfwInit()) {
+		std::cout << "Failed to initialize GLFW." << std::endl;
+		return -1;
+	}
+
+	/*GLFW VERSION*/
+	std::cout << "GLFW Version: " << glfwGetVersionString() << std::endl;
+	glfwGetVersion(&major, &minor, &rev);
+	printf("GLFW MAJOR: %d, MINOR: %d, REV: %d\n", major, minor, rev);
+
+	/*Set OpenGL context hints.*/
+	glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+	glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+	glfwTerminate();
+
+	system("pause");
 	return 0;
 }
---
+++ opengl/opengl-examples/hello-rectangle/shaders.h
@@ -0,0 +1,4 @@
+#pragma once
+#ifndef __SHADERS__
+
+#endif
